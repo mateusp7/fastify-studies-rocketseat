73e63090e3ef43691cc4f1e642a4625e46d5fadc
mateusp7 <mateusdev1337@gmail.com>
2024-07-28 09:35:29 -0300

    feat: create post and get endpoints to transactions

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,21 @@
 import fastify from "fastify";
 import { knex } from "./database";
+import crypto from 'node:crypto'
 
 const app = fastify();
 
-app.get("/", async (req, res) => {
-  const tables = await knex('sqlite_schema').select('*')
+app.get('/transactions', async (req, res) => {
+  const tables = await knex('transactions').select('*')
   return tables
+});
+
+app.post("/transactions", async (req, res) => {
+  const transaction = await knex("transactions").insert({
+    id: crypto.randomUUID(),
+    title: 'Transação de teste',
+    amount: 1000,
+  }).returning("*")
+  return transaction;
 });
 
 app
