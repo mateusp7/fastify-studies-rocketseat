d4aab8da3b5bc69be803034b9d5a76c69fa98164
mateusp7 <mateusdev1337@gmail.com>
2024-07-28 09:42:13 -0300

    feat: create environments and install dotenv package

--- src/database.ts
+++ src/database.ts
@@ -1,15 +1,20 @@
-import { knex as setupKnex, Knex } from "knex";
+import "dotenv/config";
+import { Knex, knex as setupKnex } from "knex";
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL env not found. Please set if in .env file");
+}
 
 export const knexConfig: Knex.Config = {
   client: "sqlite",
   connection: {
-    filename: "./db/app.db",
+    filename: process.env.DATABASE_URL,
   },
   useNullAsDefault: true,
   migrations: {
     extension: "ts",
-    directory: "./db/migrations"
-  }
+    directory: "./db/migrations",
+  },
 };
 
 export const knex = setupKnex(knexConfig);
