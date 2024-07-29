ec866d7b3f842c905ef0ac61bf29042dbdeea094
mateusp7 <mateusdev1337@gmail.com>
2024-07-29 08:38:29 -0300

    feat: create transaction post with zod validator

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,20 +1,31 @@
 import { FastifyInstance } from "fastify";
+import { z } from "zod";
 import { knex } from "../database";
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get("/transactions", async () => {
+  app.post("/", async (request, reply) => {
+
+    const createTransactionBodySchema = z.object({
+      title: z.string().min(1),
+      amount: z.number().min(1),
+      type: z.enum(["credit", "debit"]),
+    })
+
+    const body = createTransactionBodySchema.parse(request.body);
+
+    const { title, amount, type } = body;
+    await knex("transactions")
+      .insert({
+        id: crypto.randomUUID(),
+        title,
+        amount: type === "credit" ? amount : amount * -1,
+      })
+      
+    return reply.status(201).send('Transação criada com sucesso!')
+  });
+  app.get("/", async () => {
     const tables = await knex("transactions").select("*");
     return tables;
   });
 
-  app.post("/transactions", async () => {
-    const transaction = await knex("transactions")
-      .insert({
-        id: crypto.randomUUID(),
-        title: "Transação de teste",
-        amount: 1000,
-      })
-      .returning("*");
-    return transaction;
-  });
 }
