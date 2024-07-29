36721d1e27d4c6ab0eb027a17696df3bf217143b
mateusp7 <mateusdev1337@gmail.com>
2024-07-29 13:57:34 -0300

    feat: add describe in test, and switch from test to it

---
+++ test/transactions.spec.ts
@@ -0,0 +1,24 @@
+import request from "supertest";
+import { afterAll, beforeAll, describe, it } from "vitest";
+import { app } from "../src/app";
+
+describe("Transactions Routes", () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it("should be able to create a new transaction", async () => { it
+    await request(app.server)
+      .post("/transactions")
+      .send({
+        title: "New Transaction",
+        amount: 5000,
+        type: "credit",
+      })
+      .expect(201);
+  });
+});
