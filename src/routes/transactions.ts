4a8cd8066dd74b6d591cb6e196bf7b552c0d0386
mateusp7 <mateusdev1337@gmail.com>
2024-07-29 08:53:03 -0300

    feat: create endpoint to get transaction by id

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -3,29 +3,39 @@ import { z } from "zod";
 import { knex } from "../database";
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.post("/", async (request, reply) => {
+  app.get("/", async () => {
+    const transactions = await knex("transactions").select();
+    return { transactions };
+  });
 
+  app.get('/:id', async (request) => {
+    const getTransactionByIdSchema = z.object({
+      id: z.string().uuid()
+    })
+
+    const { id } = getTransactionByIdSchema.parse(request.params);
+
+    const transaction = await knex("transactions").where("id", id).first();
+
+    return { transaction };
+  })
+
+  app.post("/", async (request, reply) => {
     const createTransactionBodySchema = z.object({
       title: z.string().min(1),
       amount: z.number().min(1),
       type: z.enum(["credit", "debit"]),
-    })
+    });
 
     const body = createTransactionBodySchema.parse(request.body);
 
     const { title, amount, type } = body;
-    await knex("transactions")
-      .insert({
-        id: crypto.randomUUID(),
-        title,
-        amount: type === "credit" ? amount : amount * -1,
-      })
-      
-    return reply.status(201).send('Transação criada com sucesso!')
-  });
-  app.get("/", async () => {
-    const tables = await knex("transactions").select("*");
-    return tables;
-  });
+    await knex("transactions").insert({
+      id: crypto.randomUUID(),
+      title,
+      amount: type === "credit" ? amount : amount * -1,
+    });
 
+    return reply.status(201).send("Transação criada com sucesso!");
+  });
 }
